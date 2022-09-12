ba7d1ea90febdfd325d85347f62ffd2ec723bfa7
Ezabulbary <ezabul.bary@gmail.com>
2022-09-12 22:33:51 +0600

    contact section update

--- src/pages/Shared/NavBar.js
+++ src/pages/Shared/NavBar.js
@@ -22,7 +22,6 @@ const NavBar = () => {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
-        <li><Link to='/contact'>Contact</Link></li>
         {
             user && <li><Link to='/dashboard'>Dashboard</Link></li>
         }
