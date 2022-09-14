10cbde03ad3cdf319677c91dd76a1890f5a56464
Ezabulbary <ezabul.bary@gmail.com>
2022-09-14 22:42:18 +0600

    payment card added

--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L6ZfSFK5FDe5DbuGUcsdzOGcyJVlENgvxJY6K7OVd9ykrlO99P928zUYYXqzkr4y5C4gZK6fGwseaRJ2MeG4i4U008gA7X3jL');
+
 
 const Payment = () => {
     const { id } = useParams();
@@ -22,15 +28,16 @@ const Payment = () => {
         <div className='m-10'>
             <div className="card w-50 max-w-md bg-base-100 shadow-xl my-10">
                 <div className="card-body">
-                    <h3>Hello, {order.customerName}</h3>
-                    <h2 className="card-title">Please Pay for {order.tools}</h2>
-                    <p></p>
-                    <p>Please Pay {order.pricePerPiece}</p>
+                    {/* <h3>Hello, {order.name}</h3> */}
+                    <h2 className="card-title">Please Pay for </h2>
+                    {/* <p>Please Pay {order.price}</p> */}
                 </div>
             </div>
             <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div className="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
